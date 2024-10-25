5d29334a804497180c5c52e4129d08e4d31a09b8
Adailo Melo <dev.adailo@gmail.com>
2024-10-25 12:26:22 -0300

    New folder name

--- formacao-js-backend/005_arrays/004_exercicios/ex002.js
+++
@@ -1,6 +0,0 @@
-//Crie um programa em Node.js que inicie com um array cinco elementos e atribua um novo valor para a primeira posição (índice 0) desse array. Em seguida, exiba o array antes e depois da alteração.
-
-let array = [5, 10, 15, 20, 25]
-console.log(`Antes: ${array}`)
-array[0] = 1 
-console.log(`Depois ${array}`)
--- formacao-js-backend/005_arrays/004_exercicios/ex004.js
+++
@@ -1,9 +0,0 @@
-//Desenvolva um programa em Node.js que crie um array vazio e atribua valores a ele utilizando o método push(). Adicione três números inteiros ao array e, em seguida, exiba o array resultante.
-
-let meuArray = []
-
-meuArray.push(21)
-meuArray.push(2)
-meuArray.push(4)
-
-console.log(meuArray)
