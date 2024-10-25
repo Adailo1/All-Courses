94b6e504cde4d58f865452944812bc869bc3844d
Adailo Melo <dev.adailo@gmail.com>
2024-10-25 12:26:39 -0300

    Splice method

---
+++ formacao-js-backend/005_arrays/005_splice/script.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+//Ana e Caio mudaram de escola, e Rodrigo entrou nessa sala
+
+listaEstudantes.splice(1, 2, 'Rodrigo')
+//listaEstudantes.push('Rodrigo')
+console.log(listaEstudantes)
