01b960c03ea439234a3346bd2df671b9b64e77b8
Adailo Melo <dev.adailo@gmail.com>
2024-10-27 16:08:50 -0300

    Objects inside objects

---
+++ formacao-js-backend/010_manipulando_objetos/001_objetos_aninhados/script.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: "José Silva",
+    idade: 32,
+    cpf: "12312312312",
+    turma: "JavaScript",
+    bolsista: false,
+    telefones: [ "551199999998", "551199999993" ],
+    endereco: { rua: "Rua Joseph Climber", numero: "45", complemento: "apto 43" }
+}
+
+console.log(pessoa.endereco.rua)
