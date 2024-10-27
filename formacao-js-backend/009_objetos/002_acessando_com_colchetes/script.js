113850c24cddd677ad9b06f700c9275a0f450b71
Adailo Melo <dev.adailo@gmail.com>
2024-10-27 15:42:10 -0300

    Another way to use objects

---
+++ formacao-js-backend/009_objetos/002_acessando_com_colchetes/script.js
@@ -0,0 +1,13 @@
+const estudante = {
+    nome: 'Adailo',
+    idade: 21,
+    cpf: 12345678,
+    linguagens: ['JavaScript', 'Python']
+}
+
+console.log(estudante)
+console.log(`
+Nome: ${estudante['nome']}
+Idade: ${estudante['idade']}
+Linguagens estudando/estudadas: ${estudante['linguagens']}
+`)
