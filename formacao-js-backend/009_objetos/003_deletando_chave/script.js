194617bdd31cf5b9ededf9207cc7ab57f0840cf3
Adailo Melo <dev.adailo@gmail.com>
2024-10-27 15:42:26 -0300

    Deleting an object key

---
+++ formacao-js-backend/009_objetos/003_deletando_chave/script.js
@@ -0,0 +1,17 @@
+const estudante = {
+    nome: 'Adailo',
+    idade: 21,
+    cpf: 12345678,
+    linguagens: ['JavaScript', 'Python']
+}
+
+console.log(estudante)
+console.log(`
+Nome: ${estudante.nome}
+Idade: ${estudante.idade}
+Linguagens estudando/estudadas: ${estudante.linguagens}
+`)
+
+delete estudante.linguagens
+console.log(estudante)
+console.log(estudante.linguagens)
