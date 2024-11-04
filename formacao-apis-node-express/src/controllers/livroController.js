7eefad63f8f1e1f2566757b9f5d49a434d0db5b9
Adailo Melo <dev.adailo@gmail.com>
2024-11-04 19:33:56 -0300

    Fixing import

--- formacao-apis-node-express/src/controllers/livroController.js
+++ formacao-apis-node-express/src/controllers/livroController.js
@@ -1,10 +1,19 @@
-import livro from "../models/Livro"
+import livro from "../models/Livro.js"
 
 class LivroController {
     
     static async listarLivros(req, res){
         const listaLivros = await livro.find({})
         res.status(200).json(listaLivros) //Não pode usar send() com dados como objetos
+    }
+
+    static async cadastrarLivro(req, res){
+        try {
+            const novoLivro = await livro.create(req.body)
+            res.status(201).json( { message: "Livro criado com sucesso", livro: novoLivro } )
+        } catch(erro){
+            res.status(500).json( { message: `${erro.message} - falha ao cadastrar livro` } )
+        }
     }
 }
 
