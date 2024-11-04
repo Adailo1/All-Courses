73512d9da9dbdd1d66dbb75cd2acad73150c6684
Adailo Melo <dev.adailo@gmail.com>
2024-11-04 13:57:43 -0300

    Create POST: /livros

--- formacao-apis-node-express/src/app.js
+++ formacao-apis-node-express/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
 const livros = [
     { id: 1, nome: 'Percy Jackson' },
@@ -11,8 +12,13 @@ app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJS")
 })
 
-app.get("/livros", (req, res) => {
+app.get("/livros", (req, res) => { 
     res.status(200).json(livros) //Não pode usar send() com dados como objetos
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso")
 })
 
 export default app
