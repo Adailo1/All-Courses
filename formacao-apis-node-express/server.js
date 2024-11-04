ff16e0173cb818fad04284280dd64e1109bf02ca
Adailo Melo <dev.adailo@gmail.com>
2024-11-04 09:53:44 -0300

    Creating routes

--- formacao-apis-node-express/server.js
+++ formacao-apis-node-express/server.js
@@ -2,9 +2,15 @@ import http from 'http' //HyperText transfer protocol
 
 const PORT = 3000
 
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
 const server = http.createServer((req, res) => {//Requisição e Resposta
     res.writeHead(200, { "Content-Type": "text/plain"}) //Header (HTTP)
-    res.end("Curso de Node.JS") 
+    res.end(rotas[req.url]) //
 })
 
 server.listen(PORT, () => {
