c45cc08291bf87181ed52e88cc732b41f906104f
Adailo Melo <dev.adailo@gmail.com>
2024-11-04 10:21:58 -0300

    Creating route: /livros

--- formacao-apis-node-express/src/app.js
+++ formacao-apis-node-express/src/app.js
@@ -2,8 +2,17 @@ import express from "express"
 
 const app = express()
 
+const livros = [
+    { id: 1, nome: 'Percy Jackson' },
+    { id: 2, nome: 'Meu sol de primavera' }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJS")
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros) //Não pode usar send() com dados como objetos
 })
 
 export default app
