5fd5a769a705a27171029150fe47b9b05047f974
Adailo Melo <dev.adailo@gmail.com>
2024-11-05 19:34:35 -0300

    Deleting older CRUD

--- formacao-apis-node-express/src/app.js
+++ formacao-apis-node-express/src/app.js
@@ -15,21 +15,6 @@ conexao.once("open", () => {
 const app = express()
 routes(app)
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id) 
-    res.status(200).json(livros[index])
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body)
-})
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id)
-    livros[index].nome = req.body.nome
-    res.status(200).json(livros)
-})
-
 app.delete("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id)
     livros.splice(index, 1)
